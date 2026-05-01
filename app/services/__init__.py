193829c4e60946fcfa105314886687325a5a5d6d
akapzg <akapzg@gmail.com>
2026-05-01 19:55:36 +0800

    chore: update pyproject.toml and remaining file headers with current authorship

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-@Time    : 2025/7/16 21:34
-@Author  : QIN2DIM
-@GitHub  : https://github.com/QIN2DIM
-@Desc    :
+Epic Games Free Game Collection - Services Initializer
+
+@Time    : 2026/05/01
+@Author  : akapzg
+@GitHub  : https://github.com/akapzg/epic-gamer-gemini
 """
